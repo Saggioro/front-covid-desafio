af861eaf19ed27e4e4bfa257ce9a5c3e998458da
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-20 11:09:37 -0300

    feat: add user login page

---
+++ src/pages/UserLogin/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Page from '../../components/Page';
+import SignIn from '../../components/SignIn';
+
+const UserLogin = () => {
+  return (
+    <Page title="Login">
+      <SignIn />
+    </Page>
+  );
+};
+export default UserLogin;
