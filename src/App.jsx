c7d5a6210e9b484391b4399df3f7ecce001295c4
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-22 00:01:57 -0300

    feat: add toast container to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
+import { AuthProvider } from './hooks/auth';
 
 import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <Routes />
-      </BrowserRouter>
-    </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
