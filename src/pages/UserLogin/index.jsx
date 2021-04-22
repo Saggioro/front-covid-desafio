f2675ba3fafbcde2df7ae105c6242416e6c46fea
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-22 00:05:02 -0300

    feat: add toast to failed login

--- src/pages/UserLogin/index.jsx
+++ src/pages/UserLogin/index.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import Page from '../../components/Page';
 import SignIn from '../../components/SignIn';
+import { useAuth } from '../../hooks/auth';
 
 const UserLogin = () => {
+  const { signIn } = useAuth();
+
+  const history = useHistory();
+  const formik = useFormik({
+    initialValues: {
+      cpf: '',
+      password: '',
+    },
+    onSubmit: async values => {
+      try {
+        await signIn(values);
+
+        history.push('/userAppointment');
+      } catch (err) {
+        toast.error(err?.response?.data?.message);
+      }
+    },
+  });
+
   return (
     <Page title="Login">
-      <SignIn />
+      <SignIn
+        onSubmit={formik.handleSubmit}
+        cpf={formik.values.cpf}
+        password={formik.values.password}
+        handleChange={formik.handleChange}
+      />
     </Page>
   );
 };
