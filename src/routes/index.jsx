db7b375d8587eb6fd485f253e81313cecec32e29
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 20:02:35 -0300

    feat: add isPrivate atribute to routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import UserAppointment from '../pages/UserAppointment';
+import UserSignUp from '../pages/UserSignUp';
 import UserLogin from '../pages/UserLogin';
+import UserDashboard from '../pages/UserDashboard';
 
 export const routes = [
   {
     name: 'Login',
     path: '/',
     component: UserLogin,
+    isPrivate: false,
   },
   {
     name: 'Agendamento',
     path: '/userAppointment',
     component: UserAppointment,
+    isPrivate: true,
+  },
+  {
+    name: 'Cadastro',
+    path: '/userSignUp',
+    component: UserSignUp,
+    isPrivate: false,
+  },
+  {
+    name: 'Dashboard',
+    path: '/userDashboard',
+    component: UserDashboard,
+    isPrivate: true,
   },
 ];
 
 const Routes = () => {
   return (
     <Switch>
-      {routes.map(({ component, path }) => {
-        return <Route exact key={path} path={path} component={component} />;
+      {routes.map(({ component, path, isPrivate }) => {
+        return (
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={component}
+            isPrivate={isPrivate}
+          />
+        );
       })}
     </Switch>
   );
