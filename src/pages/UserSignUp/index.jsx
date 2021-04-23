89ea52c5374d09ec6b67d0ebf113448f4b7c12e4
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 20:10:10 -0300

    feat: add signUp component

---
+++ src/pages/UserSignUp/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignUp from '../../components/SignUp';
+import Page from '../../components/Page';
+
+const UserSignUp = () => {
+  return (
+    <Page title="Cadastro">
+      <SignUp />
+    </Page>
+  );
+};
+
+export default UserSignUp;
