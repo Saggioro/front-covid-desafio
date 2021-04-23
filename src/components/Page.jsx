d08162020e8f71bd372dbac4ed8bac1f3629f90a
Saggioro <diegosaggiorobrito@gmail.com>
2021-04-23 20:04:07 -0300

    feat: add button to logout from private routes

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Button, Row } from 'react-bootstrap';
 
-const Page = ({ title, children }) => {
+import { useAuth } from '../hooks/auth';
+
+const Page = ({ title, logOut = false, children }) => {
+  const { signOut } = useAuth();
   return (
-    <Container fluid>
-      <Card className="mt-10">
+    <Container style={{ minWidth: '280px' }}>
+      <Card>
         <Card.Header>
-          <Card.Title>{title}</Card.Title>
+          <Row className="d-flex justify-content-between">
+            <Card.Title className="mt-10">{title}</Card.Title>
+
+            {logOut && <Button onClick={signOut}>Sair</Button>}
+          </Row>
         </Card.Header>
+
         <Card.Body>{children}</Card.Body>
       </Card>
     </Container>
